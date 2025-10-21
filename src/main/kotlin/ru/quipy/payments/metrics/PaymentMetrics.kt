0a4efd69f357dab47555c40a832e30fe1d19d220
notoriginallink <toldan04@yandex.ru>
2025-10-21 23:38:24 +0300

    hw-3: add payments count metrics and update dashboard

--- src/main/kotlin/ru/quipy/payments/metrics/PaymentMetrics.kt
+++ src/main/kotlin/ru/quipy/payments/metrics/PaymentMetrics.kt
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Service
 class PaymentMetrics(
     private val meterRegistry: MeterRegistry,
 ) {
+    private companion object {
+        const val SUCCESS_PAYMENT_TAG = "success"
+        const val FAILED_PAYMENT_TAG = "failed"
+        const val TIMEOUT_PAYMENT_TAG = "timeout"
+    }
+
     fun incIncomingTotal(url: String) = Counter
         .builder("incoming_http_requests")
         .description("Number of incoming requests")
@@ -33,6 +39,22 @@ class PaymentMetrics(
             "account", account,
             "response_code", responseCode,
             "response_desc", responseDesc,
+        )
+        .register(meterRegistry)
+        .increment()
+
+    fun incSuccessPayment(account: String) = incPaymentsCount(account, SUCCESS_PAYMENT_TAG)
+
+    fun incTimeoutPayment(account: String) = incPaymentsCount(account, TIMEOUT_PAYMENT_TAG)
+
+    fun incFailedPayment(account: String) = incPaymentsCount(account, FAILED_PAYMENT_TAG)
+
+    private fun incPaymentsCount(account: String, status: String) = Counter
+        .builder("payments_count")
+        .description("Number of payments")
+        .tags(
+            "account", account,
+            "status", status,
         )
         .register(meterRegistry)
         .increment()
